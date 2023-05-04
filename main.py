a9c31545f5c0974cdcb3441589937dd9c4c775ed
Timothee2010 <117906444+Timothee2010@users.noreply.github.com>
2023-05-04 18:16:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
+### creer l'index ###
 
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+### interface personne ###
+
+chaineacrypter = input("S'il te plait, entre un message a crypter :")
+chaineacrypter = chaineacrypter.upper()
+quantitedecalage = int(input("S'il te plait, entre un nombre entier de 1 a 25 en guise de cle."))
+chainecryptee = ""
+
+### fonctinnoment ###
+
+for caractereactuel in chaineacrypter:
+  position = alphabet.find(caractereactuel)
+  nouvelleposition = position + quantitedecalage
+  chainecryptee = chaineacyptee + alphabet[nouvelleposition]
+print("ton message crypte est", chainecryptee)
+
+### ponctuation ###
+
+for caractereactuel in chaineacrypter:
+  position = alphabet.find(caractereactuel)
+  nouvelleposition = position + quantitedecalage
+  if caractereactuel in alphabet:
+    chainecryptee = chainecryptee + alphabet[nouvelleposition]
+  else:
+    chainecryptee = chainecryptee + caractereactuel
